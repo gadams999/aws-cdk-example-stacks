235afca51496cf60a0df5c99213a9eb70bf63d96
Gavin Adams <gavinaws@amazon.com>
2020-07-17 10:09:44 -0600

    work on construct

--- vpc-nlb-eip/lib/cdk-nlb-static.ts
+++ vpc-nlb-eip/lib/cdk-nlb-static.ts
@@ -1,6 +1,7 @@
-import * as cdk from "@aws-cdk/core"
-import * as ec2 from "@aws-cdk/aws-ec2"
-import { CfnLoadBalancer } from "@aws-cdk/aws-elasticloadbalancingv2"
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { CfnLoadBalancer } from "@aws-cdk/aws-elasticloadbalancingv2";
+import { CfnResource } from "@aws-cdk/core";
 
 /**
  * Creates a public facing network load balancer with elastic ip addresses
@@ -8,30 +9,76 @@ import { CfnLoadBalancer } from "@aws-cdk/aws-elasticloadbalancingv2"
  */
 
 export interface NetworkLoadBalancerStaticProps {
-    /**
-     * The subnets for creating mapping
-     */
-    readonly subnetList: [ec2.ISubnet]
+  /**
+   * The subnets for creating mapping
+   */
+  readonly name: string;
+  readonly vpc: ec2.Vpc;
 }
 
+export interface ListenerProps {
+  /**
+   * Create listener for UDP, TCP, etc. and port
+   */
+  readonly name: string;
+  readonly protocol: string;
+  readonly port: number;
+  }
+
 export class NetworkLoadBalancerStatic extends cdk.Construct {
-    constructor(
-        scope: cdk.Construct,
-        id: string,
-        props: NetworkLoadBalancerStaticProps
+  eipArray: ec2.CfnEIP[] = [];
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: NetworkLoadBalancerStaticProps
+  ) {
+    super(scope, id);
+
+    // Property validation
+    if (
+      !RegExp("^[^-][da-zA-Z()-]+[^-]$").test(props.name) ||
+      props.name.length > 32
     ) {
-        super(scope, id)
-
-        // subnet
-        let subnets = []
-        for (let i = 0; i < props.subnetList.length; i++) {
-            subnets.push(props.subnetList[i])
-        }
-
-        const nlb = new CfnLoadBalancer(this, "NLB", {
-            name: id,
-            type: "network",
-            subnetMappings: subnets,
-        })
+      throw new Error(
+        "'NetworkLoadBalancerStatic' must only contain alphanumeric characters and hyphen, must not start or end with a hyphen, and be less than 32 characters in length"
+      );
     }
+
+    // For all subnets in the VPC, allocate an EIP as part
+    // of the subnet mapping
+    let subnetMappings = [];
+    let workingEip;
+    // Create eipArray first and add to array
+    for (let i = 0; i < props.vpc.publicSubnets.length; i++) {
+      workingEip = new ec2.CfnEIP(this, `EIP_${i + 1}`, {
+        domain: "vpc",
+      });
+      this.eipArray.push(workingEip);
+      subnetMappings[i] = {
+        subnetId: props.vpc.publicSubnets[i].subnetId,
+        allocationId: workingEip.attrAllocationId,
+      };
+    }
+
+    // Create the load balancer
+    const nlb = new CfnLoadBalancer(this, "NLB", {
+      name: props.name,
+      type: "network",
+      scheme: "internet-facing",
+      subnetMappings: subnetMappings,
+    });
+
+    // these may be removed once the determination how to do disaccociations of eipArray to the NLB
+    // nlb.addDependsOn(eipArray[0])
+    // nlb.addDependsOn(eipArray[1])
+    // const igw = props.vpc.node.findChild('IGW') as ec2.CfnInternetGateway
+    // const vpcResource = props.vpc.node.findChild('Resource') as CfnResource
+    // // Add dependencies to detach eipArray
+    // for (let i = 0; i < eipArray.length; i++) {
+    //     eipArray[i].addDependsOn(vpcResource)
+    // }
+  }
+  public addListener(scope: cdk.Construct, id: string, props: ListenerProps) {
+
+  }
 }
--- vpc-nlb-eip/lib/vpc-nlb-eip-stack.ts
+++ vpc-nlb-eip/lib/vpc-nlb-eip-stack.ts
@@ -1,49 +1,48 @@
-import * as cdk from "@aws-cdk/core"
-import * as ec2 from "@aws-cdk/aws-ec2"
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 // import { NetworkLoadBalancer } from "@aws-cdk/aws-elasticloadbalancingv2"
-// import { NetworkLoadBalancerStatic } from "./cdk-nlb-static"
+import { NetworkLoadBalancerStatic } from "./cdk-nlb-static";
 
 export class VpcNlbEipStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props)
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        // Create VPC with public AZs
-        // create NLB across AZs
-        // Add EIPs per AZ
-        // add listener for udp
-        // launch config
-        // target profile
-        // autoscaling 1-n
+    // Create a new VPC with public subnets
+    const vpc = new ec2.Vpc(this, "VPC", {
+      cidr: "10.0.0.0/16",
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "Public Access",
+        },
+      ],
+    });
 
-        const vpc = new ec2.Vpc(this, "VPC", {
-            cidr: "10.0.0.0/16",
-            maxAzs: 3,
-            subnetConfiguration: [
-                {
-                    subnetType: ec2.SubnetType.PUBLIC,
-                    name: "Public Access",
-                },
-            ],
-        })
+    // Create a network load balancer with an elastic IP address in
+    // each availability zone.
+    const nlb = new NetworkLoadBalancerStatic(this, "NLB", {
+      name: "MyStaticNLB",
+      vpc: vpc,
+    });
 
-        // NLB
-        // const nlb = new NetworkLoadBalancerStatic(this, "NLB", {
-        //   subnetList: vpc.publicSubnets
-        // })
-
-        // const nlb = new NetworkLoadBalancer(this, "NLB", {
-        //     vpc: vpc,
-        //     loadBalancerName: "NLB",
-        //     crossZoneEnabled: true,
-        //     deletionProtection: false,
-        //     internetFacing: true,
-        // })
-
-        // Create the EIPs for the NLB
-        // for (let i = 0; i < vpc.availabilityZones.length; i++) {
-        //   nlb.node
-        // }
-        //   console.log("AZs:", vpc.availabilityZones.length)
-        // }
+    // Make the EIP addresses available CloudFormation outputs
+    for (let i = 0; i < nlb.eipArray.length; i++) {
+      new cdk.CfnOutput(this, `EIP${i + 1}`, {
+        description: `Elastic IP Address ${i + 1}`,
+        value: nlb.eipArray[i].ref,
+      });
     }
+
+    /**
+     * Below is an example to create an NLB listener -> target group -> ASG -> Instances
+     */
+
+    // launch config
+    // create an ASG
+    // create target group
+    // add listener for udp
+    // target profile
+    // autoscaling 1-n
+  }
 }
