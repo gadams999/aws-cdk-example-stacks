af186c92d119b04fdd368059c9f6a64d9ffe376a
Gavin Adams <gavinaws@amazon.com>
2020-07-20 21:00:46 -0600

    remove uneeded definitions

--- vpc-nlb-eip/lib/cdk-nlb-static.ts
+++ vpc-nlb-eip/lib/cdk-nlb-static.ts
@@ -1,14 +1,10 @@
-import * as cdk from "@aws-cdk/core"
-import * as ec2 from "@aws-cdk/aws-ec2"
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 import {
-    CfnLoadBalancer,
-    CfnListener,
-    CfnListenerProps,
-    CfnListenerRule,
-    NetworkListenerProps,
-} from "@aws-cdk/aws-elasticloadbalancingv2"
-import * as elb2 from "@aws-cdk/aws-elasticloadbalancingv2"
-import { VpcNlbEipStack } from "./vpc-nlb-eip-stack"
+  CfnLoadBalancer,
+  CfnListener,
+  NetworkListenerProps,
+} from "@aws-cdk/aws-elasticloadbalancingv2";
 
 /**
  * Creates a public facing network load balancer with elastic ip addresses
@@ -16,85 +12,88 @@ import { VpcNlbEipStack } from "./vpc-nlb-eip-stack"
  */
 
 export interface NetworkLoadBalancerStaticProps {
-    /**
-     * The subnets for creating mapping
-     */
-    readonly name: string
-    readonly vpc: ec2.Vpc
+  /**
+   * The name of the NLB
+   */
+  readonly name: string;
+
+  /**
+   * VPC to get the AZs, generate EIPs for each
+   */
+
+  readonly vpc: ec2.Vpc;
 }
 
 export interface ListenerProps {
-    /**
-     * Create listener for UDP, TCP, etc. and port
-     */
-    readonly props: NetworkListenerProps
+  /**
+   * Create listener for UDP, TCP, etc. and port
+   */
+  readonly props: NetworkListenerProps;
 }
 
 export class NetworkLoadBalancerStatic extends cdk.Construct {
-    eipArray: ec2.CfnEIP[] = []
-    nlb: CfnLoadBalancer
-    arn: string
-    listeners: CfnListener[] = []
-    constructor(
-        scope: cdk.Construct,
-        id: string,
-        props: NetworkLoadBalancerStaticProps
+  eipArray: ec2.CfnEIP[] = [];
+  nlb: CfnLoadBalancer;
+  arn: string;
+  listeners: CfnListener[] = [];
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: NetworkLoadBalancerStaticProps
+  ) {
+    super(scope, id);
+
+    // Property validation
+    if (
+      !RegExp("^[^-][da-zA-Z()-]+[^-]$").test(props.name) ||
+      props.name.length > 32
     ) {
-        super(scope, id)
-
-        // Property validation
-        if (
-            !RegExp("^[^-][da-zA-Z()-]+[^-]$").test(props.name) ||
-            props.name.length > 32
-        ) {
-            throw new Error(
-                "'NetworkLoadBalancerStatic' must only contain alphanumeric characters and hyphen, must not start or end with a hyphen, and be less than 32 characters in length"
-            )
-        }
-
-        // For all subnets in the VPC, allocate an EIP as part
-        // of the subnet mapping
-        let subnetMappings = []
-        let workingEip
-        // Create eipArray first and add to array
-        for (let i = 0; i < props.vpc.publicSubnets.length; i++) {
-            workingEip = new ec2.CfnEIP(this, `EIP_${i + 1}`, {
-                domain: "vpc",
-            })
-            this.eipArray.push(workingEip)
-            subnetMappings[i] = {
-                subnetId: props.vpc.publicSubnets[i].subnetId,
-                allocationId: workingEip.attrAllocationId,
-            }
-        }
-
-        // Create the load balancer
-        this.nlb = new CfnLoadBalancer(this, "NLB", {
-            name: props.name,
-            type: "network",
-            scheme: "internet-facing",
-            subnetMappings: subnetMappings,
-        })
-        this.arn = this.nlb.ref
-
-        // these may be removed once the determination how to do disaccociations of eipArray to the NLB
-        // nlb.addDependsOn(eipArray[0])
-        // nlb.addDependsOn(eipArray[1])
-        // const igw = props.vpc.node.findChild('IGW') as ec2.CfnInternetGateway
-        // const vpcResource = props.vpc.node.findChild('Resource') as CfnResource
-        // // Add dependencies to detach eipArray
-        // for (let i = 0; i < eipArray.length; i++) {
-        //     eipArray[i].addDependsOn(vpcResource)
-        // }
+      throw new Error(
+        "'NetworkLoadBalancerStatic' must only contain alphanumeric characters and hyphen, must not start or end with a hyphen, and be less than 32 characters in length"
+      );
     }
 
-    public addListener(id: string, port: number, protocol: string) {
-        const listener = new CfnListener(this, id, {
-            loadBalancerArn: this.arn,
-            port: port,
-            protocol: protocol,
-            defaultActions: [{ type: "forward" }],
-        })
-        this.listeners.push(listener)
+    // For all subnets in the VPC, allocate an EIP as part
+    // of the subnet mapping
+    let subnetMappings = [];
+    let workingEip;
+    // Create eipArray first and add to array
+    for (let i = 0; i < props.vpc.publicSubnets.length; i++) {
+      workingEip = new ec2.CfnEIP(this, `EIP_${i + 1}`, {
+        domain: "vpc",
+      });
+      this.eipArray.push(workingEip);
+      subnetMappings[i] = {
+        subnetId: props.vpc.publicSubnets[i].subnetId,
+        allocationId: workingEip.attrAllocationId,
+      };
     }
+
+    // Create the load balancer
+    this.nlb = new CfnLoadBalancer(this, "NLB", {
+      name: props.name,
+      type: "network",
+      scheme: "internet-facing",
+      subnetMappings: subnetMappings,
+    });
+    this.arn = this.nlb.ref;
+
+    /**
+     * In order to have a clean stack deletion, there will need to be a custom resource
+     * to disassociate the EIP's from the NLB.
+     * 
+     * TODO: create the custom resource.
+     */
+    
+  }
+
+  public addListener(id: string, port: number, protocol: string) {
+    const listener = new CfnListener(this, id, {
+      loadBalancerArn: this.arn,
+      port: port,
+      protocol: protocol,
+      defaultActions: [{ type: "forward" }],
+    });
+    this.listeners.push(listener);
+  }
 }
--- vpc-nlb-eip/lib/vpc-nlb-eip-stack.ts
+++ vpc-nlb-eip/lib/vpc-nlb-eip-stack.ts
@@ -1,98 +1,112 @@
-import * as cdk from "@aws-cdk/core"
-import * as ec2 from "@aws-cdk/aws-ec2"
-import { AutoScalingGroup, HealthCheck } from "@aws-cdk/aws-autoscaling"
-import { ManagedPolicy } from "@aws-cdk/aws-iam"
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { AutoScalingGroup } from "@aws-cdk/aws-autoscaling";
+import { ManagedPolicy } from "@aws-cdk/aws-iam";
 import {
-    NetworkTargetGroup,
-    Protocol,
-    TargetType,
-    INetworkLoadBalancerTarget,
-    CfnListener,
-} from "@aws-cdk/aws-elasticloadbalancingv2"
-import { NetworkLoadBalancerStatic } from "./cdk-nlb-static"
-import { type } from "os"
-// import { AmazonLinuxImage } from "@aws-cdk/aws-ec2"
+  NetworkTargetGroup,
+  Protocol,
+  TargetType,
+  CfnListener,
+} from "@aws-cdk/aws-elasticloadbalancingv2";
+import { NetworkLoadBalancerStatic } from "./cdk-nlb-static";
 
 export class VpcNlbEipStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props)
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        // Create a new VPC with only public subnets
-        const vpc = new ec2.Vpc(this, "VPC", {
-            cidr: "10.0.0.0/16",
-            maxAzs: 3,
-            subnetConfiguration: [
-                {
-                    subnetType: ec2.SubnetType.PUBLIC,
-                    name: "Public Access",
-                },
-            ],
-        })
+    // Create a new VPC with only public subnets
+    const vpc = new ec2.Vpc(this, "VPC", {
+      cidr: "10.0.0.0/16",
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "Public Access",
+        },
+      ],
+    });
 
-        // Create a network load balancer with an elastic IP address in
-        // each availability zone.
-        const nlb = new NetworkLoadBalancerStatic(this, "NLB", {
-            name: "MyStaticNLB",
-            vpc: vpc,
-        })
-        const tg = new NetworkTargetGroup(this, "TargetGroup", {
-            port: 80,
-            protocol: Protocol.TCP,
-            targetType: TargetType.INSTANCE,
-            vpc: vpc,
-        })
+    // Create a network load balancer with an elastic IP address in
+    // each availability zone.
+    const nlb = new NetworkLoadBalancerStatic(this, "NLB", {
+      name: "MyStaticNLB",
+      vpc: vpc,
+    });
+    const tg = new NetworkTargetGroup(this, "TargetGroup", {
+      port: 80,
+      protocol: Protocol.TCP,
+      targetType: TargetType.INSTANCE,
+      vpc: vpc,
+    });
 
-        const listener = new CfnListener(this, "WEBTRAFFIC", {
-            loadBalancerArn: nlb.arn,
-            port: 80,
-            protocol: "TCP",
-            defaultActions: [{ type: "forward" }],
-        })
-        tg.registerListener(listener.arn)
+    const listener = new CfnListener(this, "WEBTRAFFIC", {
+      loadBalancerArn: nlb.arn,
+      port: 80,
+      protocol: "TCP",
+      defaultActions: [{ type: "forward", targetGroupArn: tg.targetGroupArn }],
+    });
 
-        // Make the EIP addresses available CloudFormation outputs
-        let i = 1
-        for (let eip of nlb.eipArray) {
-            new cdk.CfnOutput(this, `NLB_${i}`, {
-                description: `Elastic IP Address ${i++}`,
-                value: eip.ref,
-            })
-        }
-
-        // Add a listener to the NLB
-
-        /**
-         * Below is an example to create an NLB listener -> target group -> ASG -> Instances
-         */
-
-        // ASG with launch config
-        let commands = `#!/bin/bash -xe
-        yum update && yum upgrade -y
-        foo bar
-        `
-        let userData = ec2.UserData.custom(commands)
-
-        const asg = new AutoScalingGroup(this, "AutoscalingGroup", {
-            instanceType: ec2.InstanceType.of(
-                ec2.InstanceClass.BURSTABLE3,
-                ec2.InstanceSize.MEDIUM
-            ),
-            vpc: vpc,
-            machineImage: new ec2.AmazonLinuxImage({
-                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-            }),
-            desiredCapacity: 2,
-            userData: userData,
-        })
-        // Enable for management from Systems Manager
-        asg.role.addManagedPolicy(
-            ManagedPolicy.fromAwsManagedPolicyName(
-                "AmazonSSMManagedInstanceCore"
-            )
-        )
-        tg.addTarget(asg)
-        // add listener for udp
-        // target profile
-        // autoscaling 1-n
+    // Make the EIP addresses available CloudFormation outputs
+    let i = 1;
+    for (let eip of nlb.eipArray) {
+      new cdk.CfnOutput(this, `NLB_${i}`, {
+        description: `Elastic IP Address ${i++}`,
+        value: eip.ref,
+      });
     }
+
+    /**
+     * Below is an example to create an NLB listener -> target group -> ASG -> Instances
+     */
+
+    // UserData for the ASG launch config. will be executed once per-instance startup
+    let commands = `#!/bin/bash -xe
+yum update && yum upgrade -y
+yum install httpd -y
+systemctl start httpd
+`;
+    let userData = ec2.UserData.custom(commands);
+
+    // Create a security group for inbound traffic
+    // Since the NLB will present the original IP address, set inbound to
+    // proper ipv4/v6 scopes.
+    const sg = new ec2.SecurityGroup(this, "Instance SG", {
+      description: "All Load balancer to instance",
+      vpc: vpc,
+    });
+    sg.addIngressRule(
+      ec2.Peer.ipv4("0.0.0.0/0"),
+      ec2.Port.tcp(80),
+      "HTTP incoming"
+    );
+
+    // Create the ASG with t3.medium instances, AL2, and honor the NLB's 
+    // target group health checks along with normal ec2 health checks.
+    const asg = new AutoScalingGroup(this, "AutoscalingGroup", {
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MEDIUM
+      ),
+      vpc: vpc,
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      desiredCapacity: 2,
+      healthCheck: {
+          type: "ELB",
+          gracePeriod: cdk.Duration.seconds(30)
+      },
+      userData: userData,
+      securityGroup: sg,
+    });
+    // Enable the automatic management via Systems Manager
+    // by adding the policy to the instance role. Add other
+    // AWS services these instances should monitor.
+    asg.role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
+
+    // Finally register the TargetGroup in the autoscaling group
+    tg.addTarget(asg);
+  }
 }
